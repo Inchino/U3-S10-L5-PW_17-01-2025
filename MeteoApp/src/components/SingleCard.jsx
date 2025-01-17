ff801e3d6eace1ef86a869a6199d76b2b6b70628
Inchino <alessandro.incalza22@gmail.com>
2025-01-17 16:08:55 +0100

    Correzioni

--- MeteoApp/src/components/SingleCard.jsx
+++ MeteoApp/src/components/SingleCard.jsx
@@ -1,27 +1,52 @@
 import { Row, Col, Image, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const SingleCard = ({ city }) => {
-  const navigate = useNavigate(); // Hook per la navigazione
+  const navigate = useNavigate();
+  const [isFavorited, setIsFavorited] = useState(false);
+
+  const toggleFavorite = () => {
+    setIsFavorited(!isFavorited);
+  };
 
   return (
     <Row className="justify-content-center mt-4 border">
-      <Col xs={4} className="text-center">
+      <Col
+        xs={1}
+        className="text-center d-flex align-items-center justify-content-center"
+      >
+        <i
+          className={`bi ${isFavorited ? "bi-star-fill" : "bi-star"}`}
+          onClick={toggleFavorite}
+          style={{
+            fontSize: "24px",
+            cursor: "pointer",
+            color: isFavorited ? "gold" : "gray",
+          }}
+        ></i>
+      </Col>
+
+      <Col xs={3} className="text-center">
         <Image
           src={`https://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`}
           alt={city.weather[0].description}
         />
         <p>{(city.main.temp - 273.15).toFixed(2)} °C</p>
       </Col>
+
       <Col xs={5} className="d-flex align-items-center justify-content-center">
-        <h3>{city.name}</h3>
-        <p>{city.sys.country}</p>
+        <div>
+          <h3>{city.name}</h3>
+          <p>{city.sys.country}</p>
+        </div>
       </Col>
+
       <Col xs={3} className="d-flex align-items-center justify-content-center">
         <Button
           variant="info"
           className="text-white"
-          onClick={() => navigate('/city-details/' + city.id)} // Navigazione
+          onClick={() => navigate("/city-details/" + city.id)}
         >
           More Info
         </Button>
@@ -31,4 +56,3 @@ const SingleCard = ({ city }) => {
 };
 
 export default SingleCard;
-
