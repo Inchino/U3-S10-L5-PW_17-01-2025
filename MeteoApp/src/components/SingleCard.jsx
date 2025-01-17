17313ebd53a8f7d73f9250b52e8d00ed369612c4
Inchino <alessandro.incalza22@gmail.com>
2025-01-17 14:21:35 +0100

    Ricerca funzionante

--- MeteoApp/src/components/SingleCard.jsx
+++ MeteoApp/src/components/SingleCard.jsx
@@ -1,15 +1,20 @@
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Image } from "react-bootstrap";
 
-const SingleCard = ({city}) =>{
-    return (
-          <Row className="justify-content-center mt-4">
-            <Col xs={4} className="text-center">
-            <i>{city.weather.icon}</i> 
-            <p>{city.main.temp}</p>
-            </Col>
-            <Col xs={8}>{city.name}</Col>
-          </Row>
-    )
-}
+const SingleCard = ({ city }) => {
+  return (
+    <Row className="justify-content-center mt-4">
+      <Col xs={4} className="text-center">
+        <Image
+          src={`https://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`}
+          alt={city.weather[0].description}
+        />
+        <p>{(city.main.temp - 273.15).toFixed(2)} °C</p>
+      </Col>
+      <Col xs={8} className="d-flex align-items-center justify-content-center">
+        <h3 className="city-name">{city.name}</h3>
+      </Col>
+    </Row>
+  );
+};
 
 export default SingleCard;
